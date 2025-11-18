eaf53501ee0d1f1c050d7a5f806c8408e2707e23
serge-goc <goc@element-soft.com>
2025-11-18 18:08:14 +0300

    Initial commit

---
+++ ProductMicroservice/src/main/java/org/appsdeveloperblog/ws/products/ProductMicroserviceApplication.java
@@ -0,0 +1,13 @@
+    package org.appsdeveloperblog.ws.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMicroserviceApplication.class, args);
+	}
+
+}
