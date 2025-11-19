a92f425ac751f73d3b521f6a37e0f5db6291d205
serge-goc <goc@element-soft.com>
2025-11-19 23:51:01 +0300

    Add swagger-ui

--- ProductMicroservice/src/main/java/org/appsdeveloperblog/ws/products/rest/ProductController.java
+++ ProductMicroservice/src/main/java/org/appsdeveloperblog/ws/products/rest/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/products")    //http://localhost:<port>/products
 public class ProductController {
 
     @PostMapping
