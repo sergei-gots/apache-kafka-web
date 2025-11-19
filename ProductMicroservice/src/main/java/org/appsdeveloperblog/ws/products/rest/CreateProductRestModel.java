8c42940af88c22ae721a0c3f14a05498dd84e554
serge-goc <goc@element-soft.com>
2025-11-19 16:40:39 +0300

    Add POST createProduct method

---
+++ ProductMicroservice/src/main/java/org/appsdeveloperblog/ws/products/rest/CreateProductRestModel.java
@@ -0,0 +1,34 @@
+package org.appsdeveloperblog.ws.products.rest;
+
+import java.math.BigDecimal;
+
+public class CreateProductRestModel {
+
+    String title;
+    BigDecimal price;
+    Integer quantity;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
