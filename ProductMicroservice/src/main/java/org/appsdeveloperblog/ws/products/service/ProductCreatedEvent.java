b9b385744540298dab56fa2ddd07627911fcc31d
serge-goc <goc@element-soft.com>
2025-11-20 23:11:41 +0300

    Add ProductCreatedEvent

---
+++ ProductMicroservice/src/main/java/org/appsdeveloperblog/ws/products/service/ProductCreatedEvent.java
@@ -0,0 +1,21 @@
+package org.appsdeveloperblog.ws.products.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCreatedEvent {
+
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+}
