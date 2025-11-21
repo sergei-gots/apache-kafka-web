2af7a9c1201c5af8dbf0cadf81170e73374230d3
serge-goc <goc@element-soft.com>
2025-11-21 15:24:02 +0300

    Send message asynchronously

--- ProductMicroservice/src/main/java/org/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
+++ ProductMicroservice/src/main/java/org/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
@@ -1,13 +1,51 @@
 package org.appsdeveloperblog.ws.products.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.appsdeveloperblog.ws.products.rest.CreateProductRestModel;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    private final KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
-    public String createProduct(CreateProductRestModel product) {
-        return null;
+    public String createProduct(CreateProductRestModel productRestModel) {
+
+        String productId = UUID.randomUUID().toString();
+
+        // TODO: Persist Product Details into database table before publishing an Event
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId,
+                productRestModel.getTitle(),
+                productRestModel.getPrice(),
+                productRestModel.getQuantity()
+        );
+
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future =
+            kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
+
+        future.whenComplete((result, exception) -> {
+
+            if (exception != null) {
+                log.error("*** Failed to send message", exception);
+            } else {
+                log.info("*** Message sent successfully: {}", result.getRecordMetadata());
+            }
+        });
+
+        log.info("*** Returning product id: {}", productId);
+
+        return productId;
     }
 }
