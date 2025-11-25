4a567b7b04aac14b8222cf60cb3c684c79e1abdf
serge-goc <goc@element-soft.com>
2025-11-25 16:00:51 +0300

    Add EmailNotification Microservice App Project Module

---
+++ EmailNotificationMicroservice/src/test/java/org/appsdeveloperblog/ws/emailnotification/EmailNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.appsdeveloperblog.ws.emailnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
