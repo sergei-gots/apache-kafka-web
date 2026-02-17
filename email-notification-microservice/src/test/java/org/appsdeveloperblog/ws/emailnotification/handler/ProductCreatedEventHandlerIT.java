ddf9588705813d6b5bb6afb8f13f97d5d23621c9
gots <sergei.gots@yahoo.com>
2026-02-17 20:00:38 +0300

    Add ITest#shouldSucceed: Arrange-section

--- email-notification-microservice/src/test/java/org/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandlerIT.java
+++ email-notification-microservice/src/test/java/org/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandlerIT.java
@@ -1,15 +1,50 @@
 package org.appsdeveloperblog.ws.emailnotification.handler;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.appsdeveloperblog.ws.core.events.ProductCreatedEvent;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @EmbeddedKafka
 @SpringBootTest(properties = "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}")
 class ProductCreatedEventHandlerIT {
 
+    @DisplayName("handle should succeed")
     @Test
-    public void whenNothing_thenOk() {
+    public void shouldSucceed() {
+
+        // Arrange
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
+        productCreatedEvent.setTitle("Test Product");
+        productCreatedEvent.setProductId(UUID.randomUUID().toString());
+        productCreatedEvent.setPrice(BigDecimal.TEN);
+        productCreatedEvent.setQuantity(1);
+
+        String messageId = UUID.randomUUID().toString();
+        String messageKey = productCreatedEvent.getProductId();
+
+        ProducerRecord<String, ProductCreatedEvent> record = new ProducerRecord<>(
+                "product-created-events-topic",
+                messageKey,
+                productCreatedEvent
+        );
+
+        record.headers().add("messageId", messageId.getBytes());
+        record.headers().add(KafkaHeaders.RECEIVED_KEY, messageKey.getBytes());
+
+
+        // Act
+
+
+        // Assert
+
+
 
     }
 }
